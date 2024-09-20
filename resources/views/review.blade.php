f5ccf8889f4443a2efbf6091bf112484c6815131
Marian Figula <majofigulam3@gmail.com>
2024-09-20 10:44:05 +0200

    feat: added green border

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -8,32 +8,34 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <img src="" alt="Triad Logo">
-                </li>
-                <li>
-                    <a href="#">Chcem feedback</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <div class="green-frame">
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <img src="" alt="Triad Logo">
+                    </li>
+                    <li>
+                        <a href="#">Chcem feedback</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
 
-    <section class="bg-white">
-        <div class="section-content-wrapper">
-            <h1>Review</h1>
-            <h1>ktoré ťa posunie</h1>
-        </div>
-        <div class="section-content-wrapper">
-            <h1>Vždy si túžil po feedbacku, ktorý ťa posunie?
-                Tento ťa môže poslať na kávu s Wosom,
-                <span class="text-green">
-                alebo až do Devína.
-            </span></h1>
-        </div>
+        <section class="bg-white">
+            <div class="section-content-wrapper">
+                <h1>Review</h1>
+                <h1>ktoré ťa posunie</h1>
+            </div>
+            <div class="section-content-wrapper">
+                <h1>Vždy si túžil po feedbacku, ktorý ťa posunie?
+                    Tento ťa môže poslať na kávu s Wosom,
+                    <span class="text-green">
+                    alebo až do Devína.
+                </span></h1>
+            </div>
 
-    </section>
+        </section>
+    </div>
     </body>
 </html>
