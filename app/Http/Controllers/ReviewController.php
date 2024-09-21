c37f4aa946e29e30e7cb630b1d284266324256e2
Marian Figula <majofigulam3@gmail.com>
2024-09-21 15:07:32 +0200

    fix: using DB::transaction to prevent storing the file when user is not created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreReviewRequest;
 use App\Models\User;
 use Dotenv\Util\Str;
+use Illuminate\Support\Facades\DB;
 
 class ReviewController extends Controller
 {
@@ -14,19 +15,22 @@ class ReviewController extends Controller
 
     // TODO: stricter validation
     public function store(StoreReviewRequest $request) {
-        if ($request->hasFile('file')) {
-            $file = $request->file('file');
-            $filePath = $file->store('uploads', 'public');
-        }
+        DB::transaction(function () use ($request) {
+            $filePath = null;
 
-        User::create([
-            'name' => $request->validated()['name'],
-            'email' => $request->validated()['email'],
-            'project_description' => $request->validated()['project_description'],
-            'agreed' => $request->validated()['agreed'] ? 1 : 0,
-            'file_path' => $filePath ?? null,
-        ]);
+            if ($request->hasFile('file')) {
+                $file = $request->file('file');
+                $filePath = $file->store('uploads', 'public');
+            }
 
+            User::create([
+                'name' => $request->validated()['name'],
+                'email' => $request->validated()['email'],
+                'project_description' => $request->validated()['project_description'],
+                'agreed' => $request->validated()['agreed'] ? 1 : 0,
+                'file_path' => $filePath,
+            ]);
+        });
         return redirect()->route('review.index')->with('success', 'Review submitted successfully!');
     }
 }
