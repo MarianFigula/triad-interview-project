853ed185044459c5429bb538571eb6c13c7318ed
Marian Figula <majofigulam3@gmail.com>
2024-09-21 10:49:34 +0200

    feat: added columns to users table

--- database/migrations/2024_09_21_084419_create_users_table.php
+++ database/migrations/2024_09_21_084419_create_users_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->text('project_description');
+            $table->boolean('agreed');
             $table->timestamps();
         });
     }
