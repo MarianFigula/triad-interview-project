cec24717cc47c8be197c07b6e5c5728a07bbdd6d
Marian Figula <majofigulam3@gmail.com>
2024-09-21 09:16:22 +0200

    feat: changed returning to view using ReviewController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('review');
-});
+Route::get('/', [ReviewController::class, 'index']);
 
